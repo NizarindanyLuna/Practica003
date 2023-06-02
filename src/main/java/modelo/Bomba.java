7844f40a9fa23b4ce03f198166dbe12f177f651e
Nizarindany González Luna <nizikyua.18@gmail.com>
2023-06-02 15:45:14 -0600

    "Se codifico clase bomba"

--- src/main/java/modelo/Bomba.java
+++ src/main/java/modelo/Bomba.java
@@ -1,13 +1,84 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Is Dahud
- */
-public class Bomba {
+public class Bomba {    
+    private int nBomba;
+    private float cBomba,aLitros;
+    private Gasolina gas;
+    
+    public Bomba(){
+        nBomba=0;
+        cBomba=0;
+        aLitros=0;
+    }
+
+    public Bomba(int nBomba, float cBomba, float aLitros, Gasolina gas) {
+        this.nBomba = nBomba;
+        this.cBomba = cBomba;
+        this.aLitros = aLitros;
+        this.gas = gas;
+    }
+    
+    public Bomba(Bomba Bomb){
+        
+    }
+
+    public void setNBomba(int nBomba) {
+        this.nBomba = nBomba;
+    }
+
+    public void setCBomba(float cBomba) {
+        this.cBomba = cBomba;
+    }
+
+    public void setALitros(float aLitros) {
+        this.aLitros = aLitros;
+    }
+
+    public void setGas(Gasolina gas) {
+        this.gas=gas;
+    }
+
+    public int getNBomba() {
+        return nBomba;
+    }
+
+    public float getCBomba() {
+        return cBomba;
+    }
+
+    public float getALitros() {
+        return aLitros;
+    }
+
+    public Gasolina getGas() {
+        return gas;
+    }
+    
+    public void IniciarBomba(int nBomba,Gasolina gas, float cBomba,float aLitros){
+        setNBomba(nBomba);
+        setALitros(aLitros);
+        setCBomba(cBomba);
+        setGas(gas);
+        
+    }
+    
+    public float InventarioGasolina(){
+        return getCBomba()-getALitros();
+    }
+    
+    public float VenderGasolina(float cantidad){
+        if(InventarioGasolina()>=cantidad){
+            setALitros(cantidad);
+            return cantidad*gas.getPrecio();
+            
+        }
+        else{
+          return 0;  
+        }
+    }
+    
+    public float VentasTotales(){
+        return getALitros()*getGas().getPrecio();
+    }
     
 }
